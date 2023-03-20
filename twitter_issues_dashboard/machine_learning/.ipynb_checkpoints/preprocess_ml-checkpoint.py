24070bc03f2fc84ff5ccb1ff98dac0e316a4deb6
john-adeojo <johnadeojo91@gmail.com>
2023-03-20 14:56:18 +0000

    added training script
    added ML training scripts

---
+++ twitter_issues_dashboard/machine_learning/.ipynb_checkpoints/preprocess_ml-checkpoint.py
@@ -0,0 +1,99 @@
+import pandas as pd
+import torch
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoTokenizer
+
+
+class DataPipeline:
+    def __init__(self, df, target_col, model, random_state=42):
+        self.df = df
+        self.model = model
+        self.target_col = target_col
+        self.random_state = random_state
+
+        # split into train and test sets
+        self.train_df, self.test_df = train_test_split(
+            self.df,
+            stratify=self.df[self.target_col],
+            test_size=0.2,
+            random_state=self.random_state
+        )
+
+        # split train into train and validation sets
+        self.train_df, self.val_df = train_test_split(
+            self.train_df,
+            stratify=self.train_df[self.target_col],
+            test_size=0.2,
+            random_state=self.random_state
+        )
+
+        # initialize tokenizer
+        self.tokenizer = AutoTokenizer.from_pretrained(model)
+
+    def get_train_data(self, batch_size=16):
+        train_dataset = TextClassificationDataset(
+            self.train_df,
+            target_col=self.target_col,
+            tokenizer=self.tokenizer,
+            max_len=514,
+            model=self.model
+        )
+        train_loader = DataLoader(train_dataset, batch_size=batch_size)
+        return train_loader
+
+    def get_val_data(self, batch_size=16):
+        val_dataset = TextClassificationDataset(
+            self.val_df,
+            target_col=self.target_col,
+            tokenizer=self.tokenizer,
+            max_len=514,
+            model=self.model
+        )
+        val_loader = DataLoader(val_dataset, batch_size=batch_size)
+        return val_loader
+
+    def get_test_data(self, batch_size=16):
+        test_dataset = TextClassificationDataset(
+            self.test_df,
+            target_col=self.target_col,
+            tokenizer=self.tokenizer,
+            max_len=514,
+            model=self.model
+        )
+        test_loader = DataLoader(test_dataset, batch_size=batch_size)
+        return test_loader
+
+
+class TextClassificationDataset(Dataset):
+    def __init__(self, df, target_col, tokenizer, max_len, model):
+        self.df = df
+        self.target_col = target_col
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.model = model
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        text = self.df.iloc[idx]['cleaned_text']
+        target = self.df.iloc[idx][self.target_col]
+
+        encoding = self.tokenizer(
+            text,
+            max_length=self.max_len,
+            padding='max_length',
+            truncation=True,
+            return_attention_mask=True,
+            return_tensors='pt'
+        )
+
+        input_ids = encoding['input_ids'].squeeze(0)
+        attention_mask = encoding['attention_mask'].squeeze(0)
+
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'target': torch.tensor(target)
+        }
---
+++ twitter_issues_dashboard/machine_learning/train_models.py
@@ -0,0 +1,47 @@
+import torch
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+
+class TransformerFineTuner:
+    def __init__(self, train_loader, val_loader, model_name, num_labels, output_dir, logging_dir):
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.model_name = model_name
+        self.num_labels = num_labels
+        self.output_dir = output_dir
+        self.logging_dir = logging_dir
+        
+    def train(self):
+        # Define model
+        model = AutoModelForSequenceClassification.from_pretrained(self.model_name, num_labels=self.num_labels)
+
+        # Define training arguments
+        training_args = TrainingArguments(
+            output_dir=self.output_dir,           
+            num_train_epochs=3,              # total number of training epochs
+            fp16=True,                       # precision choice to preserve memory on GPU
+            gradient_accumulation_steps=4,   # batch size for calculating gradients  
+            per_device_train_batch_size=16,  # batch size per device during training
+            per_device_eval_batch_size=64,   # batch size for evaluation
+            warmup_steps=500,                # number of warmup steps for learning rate scheduler
+            weight_decay=0.01,               # strength of weight decay
+            logging_dir=self.logging_dir,    # directory for storing logs
+            logging_steps=25,
+            evaluation_strategy="steps",
+            load_best_model_at_end=True,
+        )
+
+        # Define trainer
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=self.train_loader.dataset,
+            eval_dataset=self.val_loader.dataset,
+        )
+
+        # Train the model
+        trainer.train()
+
+        # Save the model and tokenizer
+        trainer.save_model(self.output_dir)
+        tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        tokenizer.save_pretrained(self.output_dir)
